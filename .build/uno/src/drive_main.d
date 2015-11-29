9620ef598d0bca99553b5ccb503b160906000ab9
jasonleethomas <jasleethomas@gmail.com>
2015-11-29 12:33:14 -0500

    cleaned ino folders

---
+++ .build/uno/src/drive_main.d
@@ -0,0 +1,12 @@
+.build/uno/src/drive_main.d .build/uno/src/drive_main.o: .build/uno/src/drive_main.cpp \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h \
+ /usr/local/share/arduino/libraries/Servo/Servo.h
